a1463c790b5688dcd0fe86a71874762189a6c0b5
Kieu Hoang Nam <namkhgch200483@fpt.edu.vn>
2025-06-05 21:35:43 +0700

    css login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -4,26 +4,31 @@ const Login = (props) =>{
         return (
            <div className="login-contanier mt-3">
            <div className="container"> 
-                 <div className="row"> 
-                        <div className="content-left col-7"> 
+                 <div className="row px-3 px-sm-0"> 
+                        <div className="content-left col-12 d-none col-sm-7 d-sm-block"> 
                           <div className='brand'>
-                                 Kieu Nam
+                                 Kieu Nam IT
                             </div>
                             <div className='detail'>
-                                 Hello 123! akdsfkhsdkjfhajksdhfjkads
+                                Kieu Nam  helps you connect and share with the people in your life.
                             </div>
                         </div>
-                        <div className="content-right col-5 d-flex flex-column gap-3 py-3">
+
+                        
+                        <div className="content-right col-sm-5 col-12 d-flex flex-column gap-3 py-3 ">
+                          <div className='brand d-sm-none'>
+                                 Kieu Nam IT
+                            </div>
                          <input type='text' class='form-control' placeholder='Email or Phone number' />
                           <input type='password' class='form-control' placeholder='Password'/>
                           <button className='btn btn-primary'> Login </button>
                           
-                          <span className='text-center'> Forgot your password ? </span>
+                          <span className='text-center'> <a className='forgot-pasword' href='#'> Forgot your password ? </a>  </span>
                           <hr />
                           <div className='text-center'> 
                            <button className='btn btn-success'> Create new account </button>
                           </div>
-                          
+                         
                         </div>
                     </div>
              </div>
