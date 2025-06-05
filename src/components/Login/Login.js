8c4bac30f6414e00e3765bddec04fff5927e22f0
Kieu Hoang Nam <namkhgch200483@fpt.edu.vn>
2025-06-05 16:42:26 +0700

    design layout login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,10 +1,32 @@
-
+import './Login.scss'
 
 const Login = (props) =>{
         return (
-           <div className="login-contanier">
-            Login
-            
+           <div className="login-contanier mt-3">
+           <div className="container"> 
+                 <div className="row"> 
+                        <div className="content-left col-7"> 
+                          <div className='brand'>
+                                 Kieu Nam
+                            </div>
+                            <div className='detail'>
+                                 Hello 123! akdsfkhsdkjfhajksdhfjkads
+                            </div>
+                        </div>
+                        <div className="content-right col-5 d-flex flex-column gap-3 py-3">
+                         <input type='text' class='form-control' placeholder='Email or Phone number' />
+                          <input type='password' class='form-control' placeholder='Password'/>
+                          <button className='btn btn-primary'> Login </button>
+                          
+                          <span className='text-center'> Forgot your password ? </span>
+                          <hr />
+                          <div className='text-center'> 
+                           <button className='btn btn-success'> Create new account </button>
+                          </div>
+                          
+                        </div>
+                    </div>
+             </div>
            </div>
         )
 }
