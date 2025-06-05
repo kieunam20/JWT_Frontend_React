90ff8d214f11d886d970d3207256d9b719f569bd
Kieu Hoang Nam <namkhgch200483@fpt.edu.vn>
2025-06-05 22:09:16 +0700

    create register

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,6 +1,10 @@
 import './Login.scss'
-
+import { useHistory } from "react-router-dom";
 const Login = (props) =>{
+   let history = useHistory();
+        const handleCreateNewAccount = ()  =>{
+          history.push("/register")
+          }
         return (
            <div className="login-contanier mt-3">
            <div className="container"> 
@@ -26,7 +30,9 @@ const Login = (props) =>{
                           <span className='text-center'> <a className='forgot-pasword' href='#'> Forgot your password ? </a>  </span>
                           <hr />
                           <div className='text-center'> 
-                           <button className='btn btn-success'> Create new account </button>
+                           <button className='btn btn-success' onClick={() => handleCreateNewAccount()}> 
+                             Create new account
+                            </button>
                           </div>
                          
                         </div>
