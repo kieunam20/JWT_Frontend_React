f946dca63d7cffc92859350f9d4a53a14e0850c9
Kieu Hoang Nam <namkhgch200483@fpt.edu.vn>
2025-06-05 15:57:02 +0700

    create  login

---
+++ src/components/Login/Login.js
@@ -0,0 +1,12 @@
+
+
+const Login = (props) =>{
+        return (
+           <div className="login-contanier">
+            Login
+            
+           </div>
+        )
+}
+
+export default Login;
