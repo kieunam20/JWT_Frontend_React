8c1b9ff240cb773dfa64354601a15b26e1101413
Kieu Hoang Nam <namkhgch200483@fpt.edu.vn>
2025-06-09 11:15:20 +0700

    done register

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -3,6 +3,7 @@ import { useHistory } from "react-router-dom";
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { toast } from 'react-toastify';
+import {registerNewUser} from '../../services/userService';
 
 const Register = (props) =>{
     const [email, setEmail] = useState("");
@@ -62,21 +63,23 @@ const Register = (props) =>{
       setObjCheckInput({...defaultValidInput,isValidConfirmPassword : false});
       return false;
     }
-
-   
     return true;
    }       
 
 
 
- const handleRegister = () =>{
+ const handleRegister = async () =>{
    let check = isValidInputs();
      if(check == true ){
-        axios.post("http://localhost:8080/api/v1/register", 
-             { email, phone, username ,password})
+     let response = await registerNewUser( email, phone, username ,password);
+     let serverData = response.data;
+     if(+serverData.EC === 0){
+      toast.success(serverData.EM);
+       history.push("/login");
+     }else{
+      toast.error(serverData.EM)
+     } 
      }
-       
-    
  }         
 
         return (
